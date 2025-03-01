5ab4c7d35cb0ae92bcd565bc52275d13af360b25
kaneeldias <kaneeldias@hotmail.com>
2025-03-01 21:21:40 +0530

    Add PrivilegedRequest class for enhanced API request handling with authorization

---
+++ src/api/PrivilegedRequest.ts
@@ -0,0 +1,13 @@
+import { APIRequest } from "@/api/Request";
+import { User } from "@/classes/User/User";
+
+export abstract class PrivilegedRequest extends APIRequest {
+    protected constructor(endpoint: string, method: string) {
+        super(endpoint, method);
+        this.addHeader("Authorization", `Bearer ${User.getAccessToken()}`);
+    }
+
+    public async fetch(body: object): Promise<object> {
+        return await super.fetch(body);
+    }
+}
--- src/api/Request.ts
+++ src/api/Request.ts
@@ -3,22 +3,26 @@ import { checkResponse } from "@/utils/http-utils";
 export abstract class APIRequest {
     private readonly endpoint: string;
     private readonly method: string;
+    private readonly headers: Map<string, string> = new Map();
 
     protected constructor(endpoint: string, method: string) {
         this.endpoint = endpoint;
         this.method = method;
+        this.addHeader("Content-Type", "application/json");
     }
 
     public async fetch(body: object): Promise<object> {
         const response = await fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/${this.endpoint}`, {
             method: this.method,
-            headers: {
-                "Content-Type": "application/json",
-            },
+            headers: Object.fromEntries(this.headers),
             body: JSON.stringify(body),
         });
 
         await checkResponse(response);
         return await response.json();
+    }
+
+    protected addHeader(key: string, value: string) {
+        this.headers.set(key, value);
     }
 }
