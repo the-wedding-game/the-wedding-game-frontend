6a6ec45f79764a9d348b3cd5f118efd5abeef882
kaneeldias <kaneeldias@hotmail.com>
2025-03-03 02:46:29 +0530

    Refactor TitleText and TitleTextSkeleton components to use consistent dimensions for improved maintainability

--- src/components/text/TitleText.tsx
+++ src/components/text/TitleText.tsx
@@ -7,12 +7,16 @@ type Props = {
 
 export default function TitleText(props: Props) {
     return (
-        <Title order={2} className={`text-gray-700`}>
+        <Title order={2} className={`text-gray-700`} {...dimensions}>
             {props.children}
         </Title>
     );
 }
 
 export function TitleTextSkeleton(props: { w: string }) {
-    return <Skeleton radius={"xl"} w={props.w} h={30} mb={20} />;
+    return <Skeleton radius={"xl"} w={props.w} {...dimensions} />;
 }
+
+const dimensions = {
+    h: 36,
+};
