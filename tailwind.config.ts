7f23c68d53fbc38b4276f4f2a25a3abb9da4c1c6
kaneeldias <kaneeldias@hotmail.com>
2025-03-03 11:43:32 +0530

    Reconfigure tailwind for a desktop first approach

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -26,6 +26,19 @@ export default {
                 appear: "appear 0.2s ease-in-out",
             },
         },
+        screens: {
+            xl: { max: "1279px" },
+            // => @media (max-width: 1279px) { ... }
+
+            lg: { max: "1023px" },
+            // => @media (max-width: 1023px) { ... }
+
+            md: { max: "767px" },
+            // => @media (max-width: 767px) { ... }
+
+            sm: { max: "639px" },
+            // => @media (max-width: 639px) { ... }
+        },
     },
     plugins: [],
 } satisfies Config;
