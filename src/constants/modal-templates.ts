80cd2dd308c373b3ec915f19d0ea6db5bb2daf1f
kaneeldias <kaneeldias@hotmail.com>
2025-03-06 23:14:12 +0530

    Clean up code associated with modals usage

--- src/constants/modal-templates.ts
+++ src/constants/modal-templates.ts
@@ -1,22 +1,25 @@
 import { OpenModalParams } from "@/components/modals/Modal";
 
-export const INVALID_CREDENTIALS_MODAL: OpenModalParams = {
-    title: "Oh no! ☹️",
-    message: "Invalid username and/or password. Please try again.",
-    type: "error",
-};
+export function getSuccessModal(message: string, redirectUrl?: string) {
+    const openModalParams: OpenModalParams = {
+        title: "Yay! 😄",
+        message: message,
+        type: "success",
+    };
 
-export const NOT_ADMIN_MODAL: OpenModalParams = {
-    title: "Oh no! ☹️",
-    message: "This user is not an admin. Try again with an admin account.",
-    type: "error",
-};
+    if (redirectUrl) openModalParams.closeAction = () => (window.location.href = "/admin/challenges");
+    return openModalParams;
+}
 
-export function getGenericErrorModal(error: Error | unknown): OpenModalParams {
+export function getErrorModal(message: string, error?: Error | unknown): OpenModalParams {
     return {
         title: "Oh no! ☹️",
-        message: "There was an unexpected error. Please try again.",
+        message: message,
         type: "error",
         additionalDetails: error instanceof Error ? error.stack : "",
     };
+}
+
+export function getGenericErrorModal(error: Error | unknown): OpenModalParams {
+    return getErrorModal("There was an unexpected error. Please try again.", error);
 }
