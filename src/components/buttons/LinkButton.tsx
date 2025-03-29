3e603573a2978e2da4c4c59646b99836829dae87
kaneeldias <kaneeldias@hotmail.com>
2025-03-29 13:41:48 +0530

    Add icons to challenge action buttons and create new challenge button

--- src/components/buttons/LinkButton.tsx
+++ src/components/buttons/LinkButton.tsx
@@ -1,16 +1,21 @@
 import { Button, MantineColor, Skeleton } from "@mantine/core";
+import { ReactNode } from "react";
 
 type Props = {
     color: MantineColor;
     children: string;
     link: string;
+    icon?: ReactNode;
 };
 
 export default function LinkButton(props: Props) {
     return (
         <a href={props.link}>
             <Button variant={"outline"} color={props.color} size={"xs"} fullWidth radius="sm" {...dimensions}>
-                {props.children}
+                <div className={`flex flex-row items-center space-x-2`}>
+                    {props.icon && <div className={`w-4`}>{props.icon}</div>}
+                    <div>{props.children}</div>
+                </div>
             </Button>
         </a>
     );
@@ -21,6 +26,6 @@ export function LinkButtonSkeleton() {
 }
 
 const dimensions = {
-    w: 70,
+    w: 90,
     h: 30,
 };
