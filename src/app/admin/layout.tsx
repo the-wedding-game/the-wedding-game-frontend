63eb374c910d016ebc989d91e93c666793e7d39b
kaneeldias <kaneeldias@hotmail.com>
2025-03-30 14:09:43 +0530

    Add AccessDenied and GenericLoading components for admin access control

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -2,11 +2,8 @@
 
 import { ReactNode } from "react";
 import { useUser } from "@/classes/User/UserHook";
-import TitleText from "@/components/text/TitleText";
-import { Card, Loader } from "@mantine/core";
-import ErrorIcon from "@/components/icons/ErrorIcon";
-import AdminLoginButton from "@/components/buttons/AdminLoginButton";
-import HorizontallyCentered from "@/components/alignment/HorizontallyCentered";
+import AccessDenied from "@/components/static/AccessDenied";
+import GenericLoading from "@/components/static/GenericLoading";
 
 type Props = {
     children: ReactNode;
@@ -14,31 +11,10 @@ type Props = {
 
 export default function AdminLayout(props: Props) {
     const { user, loading } = useUser();
-    if (loading) {
-        return (
-            <HorizontallyCentered>
-                <Loader />
-            </HorizontallyCentered>
-        );
-    }
+    if (loading) return <GenericLoading />;
 
     if (!user || !user.isAdmin()) {
-        return (
-            <HorizontallyCentered>
-                <Card shadow={"md"} className={`flex flex-col space-y-5`}>
-                    <div className={`flex flex-row items-start space-x-2`}>
-                        <ErrorIcon />
-                        <div className={`flex flex-col items-start space-y-5`}>
-                            <div className={`flex flex-col items-start space-y-2`}>
-                                <TitleText>Access Denied</TitleText>
-                                <div>You must be logged in as an administrator to view this page.</div>
-                            </div>
-                            <AdminLoginButton />
-                        </div>
-                    </div>
-                </Card>
-            </HorizontallyCentered>
-        );
+        return <AccessDenied />;
     }
 
     return <>{props.children}</>;
