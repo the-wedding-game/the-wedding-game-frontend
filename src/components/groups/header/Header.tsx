0daa637e6ffb96f864e61410981cb59b9b17e1a1
kaneeldias <kaneeldias@hotmail.com>
2025-03-30 14:50:47 +0530

    Add HamburgerMenuIcon and HeaderMenu components for navigation

--- src/components/groups/header/Header.tsx
+++ src/components/groups/header/Header.tsx
@@ -1,11 +1,15 @@
 import HeaderLinks from "@/components/groups/header/HeaderLinks";
 import HeaderTagline from "@/components/groups/header/HeaderTagline";
+import HeaderMenu from "@/components/groups/header/HeaderMenu";
 
 export default function Header() {
     return (
         <div className={`flex flex-row justify-center items-center bg-slate-800 p-5 border-b-2 border-b-gray-500`}>
             <div className={`flex w-[1200px] justify-between items-center`}>
-                <HeaderTagline />
+                <div className={`flex flex-row items-center space-x-5`}>
+                    <HeaderMenu />
+                    <HeaderTagline />
+                </div>
                 <div className={`sm:hidden`}>
                     <HeaderLinks />
                 </div>
