37e3b7384cfaf330118e431728d05319d5608da0
kaneeldias <kaneeldias@hotmail.com>
2025-06-11 01:16:57 +0530

    Add admin check to ChallengePageHeader for Edit and Delete buttons

--- src/components/groups/challenge/challenge-page/ChallengePageHeader.tsx
+++ src/components/groups/challenge/challenge-page/ChallengePageHeader.tsx
@@ -4,12 +4,17 @@ import HeadingText, { HeadingTextSkeleton } from "@/components/text/HeadingText"
 import BigPointsBadge, { BigPointsBadgeSkeleton } from "@/components/badges/BigPointsBadge";
 import EditChallengeButton from "@/components/buttons/EditChallengeButton";
 import DeleteChallengeButton from "@/components/buttons/DeleteChallengeButton";
+import { useRouter } from "next/navigation";
+import { useUser } from "@/classes/User/UserHook";
 
 type Props = {
     readonly challenge: Challenge;
 };
 
 export default function ChallengePageHeader(props: Props) {
+    const router = useRouter();
+    const { user } = useUser();
+
     return (
         <div className={`flex flex-row items-start justify-between w-full space-y-3`}>
             <div className={`flex flex-col space-y-2`}>
@@ -20,10 +25,17 @@ export default function ChallengePageHeader(props: Props) {
                 </div>
             </div>
 
-            <div className={`flex flex-row sm:flex-col items-center space-x-2 sm:space-x-0 sm:space-y-2`}>
-                <EditChallengeButton challenge={props.challenge} />
-                <DeleteChallengeButton />
-            </div>
+            {user?.isAdmin() && (
+                <div className={`flex flex-row sm:flex-col items-center space-x-2 sm:space-x-0 sm:space-y-2`}>
+                    <EditChallengeButton challenge={props.challenge} />
+                    <DeleteChallengeButton
+                        challenge={props.challenge}
+                        callback={() => {
+                            router.back();
+                        }}
+                    />
+                </div>
+            )}
         </div>
     );
 }
