0ded2109ec017ffda27ab8071e4d25ed7bc7034d
kaneeldias <kaneeldias@hotmail.com>
2025-06-11 01:09:01 +0530

    Add removeChallenge function to useAllChallenges hook for challenge removal

--- src/hooks/AllChallengesHook.ts
+++ src/hooks/AllChallengesHook.ts
@@ -2,7 +2,11 @@ import { useEffect, useState } from "react";
 import { Challenge } from "@/classes/Challenge/Challenge";
 import { ChallengeFactory } from "@/classes/Challenge/ChallengeFactory";
 
-export function useAllChallenges(includeInactive: boolean): { challenges: Challenge[] | null; loading: boolean } {
+export function useAllChallenges(includeInactive: boolean): {
+    challenges: Challenge[] | null;
+    loading: boolean;
+    removeChallenge: (challengeId: number) => void;
+} {
     const [challenges, setChallenges] = useState<Challenge[] | null>(null);
     const [loading, setLoading] = useState(true);
 
@@ -15,8 +19,15 @@ export function useAllChallenges(includeInactive: boolean): { challenges: Challe
             .finally(() => setLoading(false));
     }, [includeInactive]);
 
+    function removeChallenge(challengeId: number) {
+        setChallenges((challenges) => {
+            return challenges?.filter((challenge) => challenge.id !== challengeId) || [];
+        });
+    }
+
     return {
         challenges,
         loading,
+        removeChallenge,
     };
 }
